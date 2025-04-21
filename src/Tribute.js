0e904b542945d54cb0c5551667f9410caab1d55b
James MacWilliam <jimmy.macwilliam@gmail.com>
2025-04-21 06:54:24 -0400

    fix: Perform an empty check on filtered items #910
    - when I return an `@something` from the select template, this method
      attempts to run on the result.  This is fine if the result matches the
      value, but if I want to return the key of an item from the values set,
      this will run and the collection will be missing.

--- src/Tribute.js
+++ src/Tribute.js
@@ -518,7 +518,7 @@ class Tribute {
 
   selectItemAtIndex(index, originalEvent) {
     index = parseInt(index);
-    if (typeof index !== "number" || isNaN(index)) return;
+    if (typeof index !== "number" || isNaN(index) || !this.current.filteredItems) return;
     let item = this.current.filteredItems[index];
     let content = this.current.collection.selectTemplate(item);
 
